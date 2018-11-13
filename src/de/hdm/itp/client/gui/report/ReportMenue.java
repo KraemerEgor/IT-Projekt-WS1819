2e52cb9dcd51e82c97260624d70ab3bd20a1ad85
LeonardSchwenk <leonardschwenk@gmail.com>
2018-11-14 00:06:56 +0100

    Starting With Report GUI and added secound entry point

---
+++ src/de/hdm/itp/client/gui/report/ReportMenue.java
@@ -0,0 +1,20 @@
+package de.hdm.itp.client.gui.report;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class ReportMenue implements EntryPoint{
+	
+	VerticalPanel mainPanel = new VerticalPanel();
+	
+	Button getReportButton = new Button ("Report");
+
+	public void onModuleLoad() {
+		
+		mainPanel.add(getReportButton);
+		
+		
+	}
+
+}
