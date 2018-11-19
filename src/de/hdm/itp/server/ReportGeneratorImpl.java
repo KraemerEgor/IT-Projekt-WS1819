f9d2d4f57ef3c7bb89658e01324a9525e4cc039a
LeonardSchwenk <leonardschwenk@gmail.com>
2018-11-19 22:43:20 +0100

    added all findAll methods to create Rows with information for the Report still stuff missing administrations has to be changed

--- src/de/hdm/itp/server/ReportGeneratorImpl.java
+++ src/de/hdm/itp/server/ReportGeneratorImpl.java
@@ -1,11 +1,16 @@
 package de.hdm.itp.server;
 
 import java.util.Date;
+import java.util.Vector;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import de.hdm.itp.shared.EditorAdministration;
 import de.hdm.itp.shared.ReportGenerator;
+import de.hdm.itp.shared.bo.Comment;
+import de.hdm.itp.shared.bo.Like;
+import de.hdm.itp.shared.bo.Post;
+import de.hdm.itp.shared.bo.Subs;
 import de.hdm.itp.shared.bo.User;
 import de.hdm.itp.shared.report.AllCommentsFromUserReport;
 import de.hdm.itp.shared.report.AllLikesFromUserReport;
@@ -66,112 +71,12 @@ public class ReportGeneratorImpl extends RemoteServiceServlet implements ReportG
 		return this.admin.getUserByEmail(email);
 	}
 
-
-	@Override
 	public void setUser(User u) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
 		
 	}
 
 
-	@Override
-	public AllCommentsFromUserReport createAllCommentsFromUserReport(User u) throws IllegalArgumentException {
-		
-		//if this.getAdministration(== null){return null;}
-
-		AllCommentsFromUserReport result = new AllCommentsFromUserReport();
-
-		result.setTitel("All Ihr Kommentare");
-
-		result.setCreateDate(new Date());
-
-		Row headline = new Row();
-
-		headline.addColumn(new Column("Post des Kommentars"));
-		headline.addColumn(new Column("Kommentar"));
-		headline.addColumn(new Column("Erstellungsdatum"));
-		headline.addColumn(new Column("Änderungsdatum"));
-
-	/* 	headline.addColumn(new Column(""));
-		headline.addColumn(new Column(""));
-		headline.addColumn(new Column(""));
-		headline.addColumn(new Column(""));
-		headline.addColumn(new Column(""));
-		headline.addColumn(new Column(""));
-
-		*/
-
-		Row commentRow = new Row();
-
-
-
-		return result;
-		
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	@Override
-	public AllLikesFromUserReport createAllLikesFromUserReport(User u) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	@Override
-	public AllPostsFromUserReport createAllPostsFromUserReport(User u) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	@Override
-	public AllSubsFromUserReport createAllSubsFromUserReport(User u) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	@Override
-	public AllSubsOfUserReport createAllSubsOfUserReport(User u) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-
-	
-	/*
+		/*
 	   * ***************************************************************************
 	   * Abschnitt: Erstellen der Reports
 	   * ***************************************************************************
@@ -183,5 +88,223 @@ public class ReportGeneratorImpl extends RemoteServiceServlet implements ReportG
 	*
 	* @return the all contacts of user report
 	*/
+
+
+	@Override
+	public AllCommentsFromUserReport createAllCommentsFromUserReport(User u) throws IllegalArgumentException {
+		
+		//if this.getAdministration(== null){return null;}
+
+		AllCommentsFromUserReport result = new AllCommentsFromUserReport();
+
+		result.setTitel("All Ihre Kommentare");
+
+		result.setCreateDate(new Date());
+
+		Row headline = new Row();
+
+		headline.addColumn(new Column("Post des Kommentars"));
+		headline.addColumn(new Column("Kommentar"));
+		headline.addColumn(new Column("Erstellungsdatum"));
+		headline.addColumn(new Column("Änderungsdatum"));
+
+		result.addRow(headline);
+
+		Vector<Comment> comments = this.administration.getCommentsOf(u);
+
+		for (Comment c: comments){
+			Row commentRow = new Row();
+
+			commentRow.addColumn(new Column(String.valueOf(c.getPostId())));
+			commentRow.addColumn(new Column(String.valueOf(c.getText())));
+			commentRow.addColumn(new Column(String.valueOf(c.getCreateDate())));
+			commentRow.addColumn(new Column(String.valueOf(c.getModDate())));
+
+			result.addRow(commentRow);
+		}
+
+		return result;
+
+		/*
+			amount of comments
+		*/
+		
+	}
+
+	
+
+	public AllLikesFromUserReport createAllLikesFromUserReport(User u) throws IllegalArgumentException {
+
+				//if this.getAdministration(== null){return null;}
+
+
+		AllLikesFromUserReport result = new AllLikesFromUserReport();
+
+		result.setTitel("All Ihre likes"); // amountOfLikes
+
+		result.setCreateDate(new Date());
+
+		Row headline = new Row();
+
+		headline.addColumn(new Column("Beitragsnummer"));
+		headline.addColumn(new Column("Beitragstext"));
+		headline.addColumn(new Column("Erstellungsdatum"));
+
+		result.addRow(headline);
+
+		Vector<Like> like = this.administration.getLikesOf(u);
+
+		for (Like l: like){
+			Row likesRow = new Row();
+
+			likesRow.addColumn(new Column(String.valueOf(l.getPostId())));
+			likesRow.addColumn(new Column(String.valueOf(l.getContent())));
+			likesRow.addColumn(new Column(String.valueOf(l.getCreateDate())));
+
+			result.addRow(likesRow);
+		}
+
+		return result;
+
+
+		/*
+
+		amount of likes
+		postID
+		createDate
+		*/
+	}
+
+
+
+
+
+
+	public AllPostsFromUserReport createAllPostsFromUserReport(User u) throws IllegalArgumentException {
+
+				//if this.getAdministration(== null){return null;}
+
+
+		AllPostsFromUserReport result = new AllPostsFromUserReport();
+
+		result.setTitel("All Ihre Beiträge"); // amountOfPosts
+
+		result.setCreateDate(new Date());
+
+		Row headline = new Row();
+
+		headline.addColumn(new Column("Beitragsnummer"));
+		headline.addColumn(new Column("Beitragstext"));
+		headline.addColumn(new Column("Erstellungsdatum"));
+		headline.addColumn(new Column("Änderungsdatum"));
+
+		result.addRow(headline);
+
+		Vector<Post> post = this.administration.getPostsOf(u);
+
+		for (Post p: post){
+			Row postRow = new Row();
+
+			postRow.addColumn(new Column(String.valueOf(p.getPostId())));
+			postRow.addColumn(new Column(String.valueOf(p.getContent())));
+			postRow.addColumn(new Column(String.valueOf(p.getCreateDate())));
+			postRow.addColumn(new Column(String.valueOf(p.getModDate())));
+			
+			result.addRow(postRow);
+		}
+
+
+		return result;
+		/*
+		amount of posts
+		postId
+		createDate
+		getContent
+		getModDate
+		*/
+	}
+
+
+	
+	public AllSubsFromUserReport createAllSubsFromUserReport(User u) throws IllegalArgumentException {
+		
+				//if this.getAdministration(== null){return null;}
+
+
+		AllSubsFromUserReport result = new AllSubsFromUserReport();
+
+		result.setTitel("Alle von Ihnen abonnierten Benutzer"); // amountOfPosts
+
+		result.setCreateDate(new Date());
+
+		Row headline = new Row();
+
+		headline.addColumn(new Column("Abonnierter Benutzer"));
+		headline.addColumn(new Column("Erstellungsdatum des Abonnements"));
+
+		result.addRow(headline);
+
+		Vector<Subs> subs = this.administration.getSubsOf(u);
+
+		for (Subs s: subs){
+			Row subsRow = new Row();
+
+			subsRow.addColumn(new Column(String.valueOf(s.getTargetUser())));
+			subsRow.addColumn(new Column(String.valueOf(s.getCreateDate())));
+			
+			result.addRow(subsRow);
+		}
+
+
+		return result;
+		/*
+
+		//TODO ?? targetUserID ? TargetUser nickname ?
+		amount of Subs
+		getTargetUser
+		getCreateDate
+		*/
+	}
+
+
+	
+	public AllSubsOfUserReport createAllSubsOfUserReport(User u) throws IllegalArgumentException {
+				//if this.getAdministration(== null){return null;}
+
+
+		AllSubsOfUserReport result = new AllSubsOfUserReport();
+
+		result.setTitel("All Ihre Abonnenten"); // amountOfPosts
+
+		result.setCreateDate(new Date());
+
+		Row headline = new Row();
+
+		headline.addColumn(new Column("Abonnent"));
+		headline.addColumn(new Column("Abonniert seit"));
+
+		result.addRow(headline);
+
+		Vector<Subs> subs = this.administration.getSubsOf(u);
+
+		for (Subs s: subs){
+			Row subsRow = new Row();
+
+			subsRow.addColumn(new Column(String.valueOf(s.getTargetUser())));
+			subsRow.addColumn(new Column(String.valueOf(s.getCreateDate())));
+			
+			result.addRow(subsRow);
+		}
+
+
+		return result;
+
+		/*
+		amount of subs
+		getTargetUser
+		createDate
+		*/
+	}
+	
 
 }
