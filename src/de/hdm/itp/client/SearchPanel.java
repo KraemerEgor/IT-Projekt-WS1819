a5fee0393c68213bb82244cc01ed4e09afbcb782
Louisa <louisa.kuper2@gmail.com>
2018-11-28 14:09:35 +0100

    first commit try

--- src/de/hdm/itp/client/SearchPanel.java
+++ src/de/hdm/itp/client/SearchPanel.java
@@ -1,7 +1,32 @@
 package de.hdm.itp.client;
 
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class SearchPanel extends VerticalPanel {
+	
+	
+	public static void myprofile(String[] args) {
+		String myprofile = "My Profile";
+		System.out.println(myprofile);
+	}
+	
+	public void createbutton() {
+		//Dem neuen Button eine Aktion zuweisen
+		Button b = new Button("+"); 
+		
+	}
+		
+	
+	public static void newPost(String[] args) {
+		String newpost = "create new post";
+		System.out.println(newpost);
+	}
+	
+	public SuggestBox sb = new SuggestBox();
+	
 
+	
+	
 }
