ba006c4a59688e272aa2912a65afdec87d74f736
Egor Krämer <ek067@hdm-stuttgart.de>
2018-11-28 13:53:45 +0100

    Gui fixes and try to order the panels and divs

--- src/de/hdm/itp/client/HeaderPanel.java
+++ src/de/hdm/itp/client/HeaderPanel.java
@@ -1,7 +1,23 @@
 package de.hdm.itp.client;
 
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 
 public class HeaderPanel extends HorizontalPanel {
+	
+	Label header_lbl = new Label("Jonger bei mir läuft, mies Head");
+	
+	public void onLoad() {
+		
+		super.onLoad();
+		this.addStyleName("Head");
+		
+		header_lbl.addStyleName("label_test");
+		header_lbl.setWidth("210px");
+		this.add(header_lbl);
+		this.add(header_lbl);
+		
+	
+	}
 
 }
