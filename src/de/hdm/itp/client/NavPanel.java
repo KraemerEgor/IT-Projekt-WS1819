1a0f023390d060198f68125f7c5c201b478b6e5a
Nils Kaper <nk075@hdm-stuttgart.de>
2018-12-10 16:00:40 +0100

    Style changes in NavPanel, HTML and CSS files

--- src/de/hdm/itp/client/NavPanel.java
+++ src/de/hdm/itp/client/NavPanel.java
@@ -1,27 +1,37 @@
 package de.hdm.itp.client;
 
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.HeadingElement;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class NavPanel extends VerticalPanel {
-	Label header_lbl = new Label("Label vom NavigationsPanel");
+	
+	/**
+	 * adding the header-element for the navigation panel
+	 */
+	protected HeadingElement navHeader = Document.get().createHElement(1);
+	
+	
 	VerticalPanel vp = new VerticalPanel();
 	SearchPanel sp = new SearchPanel();
 	SubsPanel sup = new SubsPanel();
 	
 	
 	public void onLoad() {
+		
+		this.getElement().appendChild(navHeader);
+		
 		super.onLoad();
 		this.addStyleName("Nav");
+		
 		
 		sp.addStyleName("Search");
 		sup.addStyleName("Subs");
 		
-		header_lbl.addStyleName("label_test");
-		header_lbl.setWidth("210px");
-		this.add(header_lbl);
-		this.add(header_lbl);
+		navHeader.setInnerText("Navigation");
+		
 		
 		this.add(sp);
 		this.add(sup);
