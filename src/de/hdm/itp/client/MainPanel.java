aa36da2107747132e31f169a60e9f5fa04985ef5
Nils Kaper <nk075@hdm-stuttgart.de>
2018-12-12 17:50:42 +0100

    moved Panel-headers to top and adjusted some style

--- src/de/hdm/itp/client/MainPanel.java
+++ src/de/hdm/itp/client/MainPanel.java
@@ -1,10 +1,17 @@
 package de.hdm.itp.client;
 
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.HeadingElement;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ScrollPanel;
 
-public class MainPanel extends HorizontalPanel {
+public class MainPanel extends ScrollPanel {
 	
+	
+	protected HeadingElement mainHeader = Document.get().createHElement(1);
 	Label header_lbl = new Label("MainPanel");
 	
 	
@@ -13,12 +20,10 @@ public void onLoad() {
 		super.onLoad();
 		this.addStyleName("Main");
 		
-		
-		
-		header_lbl.addStyleName("label_test");
-		header_lbl.setWidth("210px");
-		this.add(header_lbl);
-		
+		this.add(new HTMLPanel("<div id=\"mainHeader\"></div>"));
+		mainHeader.setInnerHTML("My Profile");
+		mainHeader.setClassName("Header");
+		DOM.getElementById("mainHeader").appendChild(mainHeader);
 }
 
 }
