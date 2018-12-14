07e2094f1450af254d2275cd7707fc41d94b6885
Nils Kaper <nk075@hdm-stuttgart.de>
2018-12-14 15:58:26 +0100

    Added some style

--- src/de/hdm/itp/client/HeaderPanel.java
+++ src/de/hdm/itp/client/HeaderPanel.java
@@ -1,11 +1,22 @@
 package de.hdm.itp.client;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class HeaderPanel extends HorizontalPanel {
 	
-	Label header_lbl = new Label("Das ist ein Label vom HeaderPanel");
+	private Label header_lbl = new Label("Das ist ein Label vom HeaderPanel");
+	private Button reportBtn = new Button("Report-Generator");
+	private Anchor reportLink = new Anchor("Report");
+	
 	
 	public void onLoad() {
 		
@@ -16,6 +27,17 @@ public class HeaderPanel extends HorizontalPanel {
 		header_lbl.setWidth("210px");
 		this.add(header_lbl);
 		this.add(header_lbl);
+		
+		RootPanel.get("Head").add(reportBtn);
+		reportBtn.setStylePrimaryName("report");
+		
+		reportBtn.addClickHandler(new ClickHandler(){
+			public void onClick(ClickEvent event) {
+				reportLink.setHref(GWT.getHostPageBaseURL() + "FeedReports.html");
+				Window.open(reportLink.getHref(), "_blank", "");
+				}
+			});
+	
 		
 	
 	}
