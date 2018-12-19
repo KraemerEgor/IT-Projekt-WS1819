fe1294bf78ba14f1f3643fbd6d965cb1c88d5a20
Nils Kaper <nk075@hdm-stuttgart.de>
2018-12-19 12:48:58 +0100

    Some Style method added in Entry Point for application to work in local mode

--- src/de/hdm/itp/client/IT_Projekt_Gruppe4_2.java
+++ src/de/hdm/itp/client/IT_Projekt_Gruppe4_2.java
@@ -61,6 +61,10 @@ public class IT_Projekt_Gruppe4_2 implements EntryPoint {
 
 	public void onModuleLoad() {
 		
+		loadApplication();
+		
+		
+		
 		Window.alert("loginTesting");
 		
 		LoginServiceAsync loginService = GWT.create(LoginService.class);
