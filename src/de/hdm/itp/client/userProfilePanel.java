321bd502f9834ab1ca9a76d1afdbb1589f29456b
Nils Kaper <nk075@hdm-stuttgart.de>
2018-12-21 20:28:05 +0100

    finished user profile

--- src/de/hdm/itp/client/userProfilePanel.java
+++ src/de/hdm/itp/client/userProfilePanel.java
@@ -7,6 +7,7 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -25,9 +26,12 @@ public class userProfilePanel extends HorizontalPanel {
 	private Image avatar_man = new Image("man.png");
 	private Image avatar_girl = new Image("girl.png");
 	private FlexTable profile = new FlexTable();
-	private Label test = new Label("Test");
+	private Label firstname_lbl = new Label("Vorname: ");
+	private Label lastname_lbl = new Label("Nachname: ");
+	private Label email_lbl = new Label("Email: ");
 	private User currentUser = ClientsideSettings.getUser();
-	private TextBox postInput = new TextBox();
+	private TextArea postInput = new TextArea();
+	private Button submitBtn = new Button("Post veröffentlichen");
 	
 	public void onLoad() {
 		
@@ -36,13 +40,16 @@ public class userProfilePanel extends HorizontalPanel {
 		avatar_man.setStylePrimaryName("avatar");
 		
 		profile.setWidget(0, 0, avatar_man);
-		profile.getFlexCellFormatter().setColSpan(0, 0, 1);
-		profile.getFlexCellFormatter().setRowSpan(0, 0, 2);
-		profile.setWidget(0, 1, new Label(" Mein Name ist Nils Kaper"));
-		profile.setWidget(1, 1, new Label("Das hier ist meine Mail-Adresse:"));
-		profile.getFlexCellFormatter().setColSpan(0, 1, 2);
+		profile.setWidget(0, 1, firstname_lbl);
+		profile.setWidget(1, 1, lastname_lbl);
+		profile.setWidget(0, 2, email_lbl);
 		
+		profile.setWidget(2, 1, postInput);
+		postInput.setStylePrimaryName("postInput");
+		profile.getFlexCellFormatter().setColSpan(2, 1, 2);
 
+		submitBtn.setStylePrimaryName("submit");
+		profile.setWidget(2, 3, submitBtn);
 		
 		
 		this.add(this.profile);
