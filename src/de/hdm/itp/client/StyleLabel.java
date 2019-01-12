5a4530e022c146ea7cb847c8a420d7680ea15eb6
Egor Krämer <ek067@hdm-stuttgart.de>
2019-01-12 14:14:30 +0100

    Design optimize

--- src/de/hdm/itp/client/StyleLabel.java
+++ src/de/hdm/itp/client/StyleLabel.java
@@ -1,11 +1,29 @@
 package de.hdm.itp.client;
 
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Label;
 
 public class StyleLabel extends Label{
 	public StyleLabel(String content, String style){
 
 		this.setText(content);
+		this.setStyleName(style);
+		this.setStylePrimaryName(style);
+		return;
+		
+	}
+	public StyleLabel(String content, ClickHandler ch){
+
+		this.setText(content);
+		this.addClickHandler(ch);
+		
+		return;
+		
+	}
+	public StyleLabel(String content, ClickHandler ch, String style){
+
+		this.setText(content);
+		this.addClickHandler(ch);
 		this.setStyleName(style);
 		this.setStylePrimaryName(style);
 		return;
