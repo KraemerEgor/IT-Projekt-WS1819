12dd77c9a0c3613cf36ef09b3c10efabb06232ad
rober <rk060@hdm-stuttgart.de>
2019-01-16 09:23:49 +0100

    Design Changes in Report Gen Gui

--- src/de/hdm/itp/client/HeaderPanelReport.java
+++ src/de/hdm/itp/client/HeaderPanelReport.java
@@ -10,14 +10,14 @@ public class HeaderPanelReport  extends HorizontalPanel {
 	public void onLoad() {
 		
 		super.onLoad();
-		this.addStyleName("Header_Report");
+		this.setStylePrimaryName("HeaderReport");
 		
-		this.setWidth("100%");
-		this.setHeight("220px");
-		this.getElement().getStyle().setBackgroundColor("grey");
+	//	this.setWidth("100%");
+	//	this.setHeight("220px");
+	//	this.getElement().getStyle().setBackgroundColor("grey");
 		
 		header_lbl.addStyleName("label_test");
-		header_lbl.setWidth("210px");
+	//	header_lbl.setWidth("210px");
 		this.add(header_lbl);
 		this.add(header_lbl);
 		
